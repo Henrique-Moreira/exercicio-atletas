65ff264563b8c66ff7740dbdc559252d06363e8a
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-28 00:17:26 -0300

    Relatório

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -11,8 +11,10 @@ public class program {
 		Scanner sc = new Scanner(System.in);
 
 		char again, sexo;
-		String nome;
-		double altura, peso;
+		String nome, atletaMaisAlto;
+		double altura, peso, pesos = 0, alturaMulheres = 0, alturaInicial = 0, atletas, men = 0, medHomens;
+		int woman = 0;
+		atletaMaisAlto = "Nenhum";
 
 		System.out.print("Nome: ");
 		nome = sc.nextLine();
@@ -23,22 +25,41 @@ public class program {
 			System.out.println("Insira uma letra válida para sexo(M/F): ");
 			sexo = sc.next().charAt(0);
 		}
+		if (sexo == 'm' || sexo == 'M') {
+			men = men + 1;
+		} else {
+			woman = woman + 1;
+		}
 
 		System.out.print("Altura: ");
 		altura = sc.nextDouble();
-
+		if (sexo == 'f' || sexo == 'F') {
+			alturaMulheres = alturaMulheres + altura;
+		}
 		while (altura <= 0) {
 			System.out.println("Insira um valor positivo para a altura: ");
 			altura = sc.nextDouble();
+			if (sexo == 'f' || sexo == 'F') {
+				alturaMulheres = alturaMulheres + altura;
+			}
 		}
-
+		if (alturaInicial < altura) {
+			atletaMaisAlto = nome;
+			alturaInicial = altura;
+		}
 		System.out.print("Peso:");
 		peso = sc.nextDouble();
+		if (peso > 0) {
+			pesos = pesos + peso;
+		}
 		while (peso <= 0) {
 			System.out.println("Insira um valor positivo para o peso: ");
 			peso = sc.nextDouble();
+			if (peso > 0) {
+				pesos = pesos + peso;
+			}
 		}
-
+		atletas = 1;
 		System.out.print("Digitar outro atleta (S/N)? ");
 		again = sc.next().charAt(0);
 
@@ -53,26 +74,53 @@ public class program {
 				System.out.println("Insira uma letra válida para sexo(M/F): ");
 				sexo = sc.next().charAt(0);
 			}
+			if (sexo == 'm' || sexo == 'M') {
+				men = men + 1;
+			} else {
+				woman = woman + 1;
+			}
 
 			System.out.print("Altura: ");
 			altura = sc.nextDouble();
-
+			if (sexo == 'f' || sexo == 'F') {
+				alturaMulheres = alturaMulheres + altura;
+			}
 			while (altura <= 0) {
 				System.out.println("Insira um valor positivo para a altura: ");
 				altura = sc.nextDouble();
+				if (sexo == 'f' || sexo == 'F') {
+					alturaMulheres = alturaMulheres + altura;
+				}
 			}
-
+			if (alturaInicial < altura) {
+				atletaMaisAlto = nome;
+				alturaInicial = altura;
+			}
 			System.out.print("Peso:");
 			peso = sc.nextDouble();
+			if (peso > 0) {
+				pesos = pesos + peso;
+			}
 			while (peso <= 0) {
 				System.out.println("Insira um valor positivo para o peso: ");
 				peso = sc.nextDouble();
+				if (peso > 0) {
+					pesos = pesos + peso;
+				}
 			}
-
+			atletas = atletas + 1;
 			System.out.print("Digitar outro atleta (S/N)? ");
 			again = sc.next().charAt(0);
 		}
+		medHomens = men / atletas * 100;
 
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Peso médio dos atletas: %.2f%n", (pesos / atletas));
+		System.out.println("Atleta mais alto: " + atletaMaisAlto);
+		System.out.printf("Porcentagem de homens: %.1f %% %n", medHomens);
+		System.out.println("Altura média das mulheres: " + (alturaMulheres / woman));
+		System.out.println();
+		System.out.println("FIM DO PROGRAMA!");
 		sc.close();
 	}
 
